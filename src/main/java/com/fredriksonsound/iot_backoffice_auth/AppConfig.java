fe1f82b1075a641af8c022f949b18221c7dd12d3
Magnus <krummelur@gmail.com>
2020-04-19 22:17:19 +0200

    refactor

--- src/main/java/com/fredriksonsound/iot_backoffice_auth/AppConfig.java
+++ src/main/java/com/fredriksonsound/iot_backoffice_auth/AppConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 public class AppConfig {
--- src/main/java/com/fredriksonsound/iot_backoffice_auth/IotBackofficeAuthApplication.java
+++ src/main/java/com/fredriksonsound/iot_backoffice_auth/IotBackofficeAuthApplication.java
@@ -14,17 +14,9 @@
 
 package com.fredriksonsound.iot_backoffice_auth;
 
-import Controller.Tokens;
 import com.fredriksonsound.iot_backoffice_auth.model.ValidationError;
-import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class IotBackofficeAuthApplication {
